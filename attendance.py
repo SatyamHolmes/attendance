b8e66a15b63e2137cae2961784808f9909789972
satyamholmes <invinciblesatyam@gmail.com>
2018-09-21 12:56:08 +0530

    v1.0

--- attendance.py
+++ attendance.py
@@ -1,55 +1,86 @@
+import sys
+import os 
+import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 
-#elem=driver.find_element_by_id('pthdr2home')
+login=""
+Password=""
+delay=60
 
-driver=webdriver.Firefox()
+def main():
+	op=Options()
+	op.headless=True
+	driver=webdriver.Firefox(options=op)
+	driver.set_window_size(850,900)
+	try:
+		global login 
+		global Password
+		if (len(sys.argv)==1):
+			raise Exception("Enter the username and Password")
+		login=sys.argv[1]
+		Password=sys.argv[2]
+		print "Please wait. Fetching your information. Depending upon the server it may take 2-3 minutes"
+		connect(login,Password,driver)		
+		get_attendance(driver)
+	except Exception as e:
+		print(e.args)
+		driver.close()
 
-#try:
-driver.get('http://115.114.127.54:8080/psp/bitcsprd/?cmd=login&languageCd=UKE')
-elem=driver.find_element_by_xpath("//a[@title='English']")
-elem.click()
-elem=driver.find_element_by_id('userid')
-elem.send_keys("B21282")
-elem=driver.find_element_by_id('pwd')
-elem.send_keys("@bioshock")
-elem.send_keys(Keys.ENTER)
-#except Exception as e:
-#	if driver.find_element_by_id("login_error")!= "":
-#		print("Incorrect Password")
-##		print("Server Down")
-#	else:
-#		print("Unknown Exception")
-#
-#	driver.close()
-#
-try:
-	elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"pthdr2home")))
+def connect(login,Password,driver):
+	driver.get('http://115.114.127.54:8080/psc/bitcsprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.B_SS_ATTEND_ROSTER.GBL?Page=B_SS_ATTEND_ROSTER&Action=U&TargetFrameName=None')
+	elem=driver.find_element_by_xpath("//a[@title='English']")
 	elem.click()
+	elem=driver.find_element_by_id('userid')
+	elem.clear()
+	elem.send_keys(login)
+	elem=driver.find_element_by_id('pwd')
+	elem.clear()
+	elem.send_keys(Password)
+	elem.send_keys(Keys.ENTER)
+	
+	#WebDriverWait(driver,120).until(lambda x: x.execute_script("return document.readystate") != "complete")
+	WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.ID,"SSR_DUMMY_RECV1$sels$0")))
+	#time.sleep(2)
+	if(driver.title=="Oracle | PeopleSoft Enterprise Sign-in"):
+	#	WebDriverWait(driver,3).until(EC.visibility_of_element_located((By.ID,"login_error")))
+		if driver.find_element_by_id("login_error").text!= "":
+			raise Exception("Invalid username or Password")
+		elif driver.find_element_by_id("discovery_error").text!= "":
+			raise Exception("Server Down")
+		else:
+			raise Exception("Unknown Error")
 
-	elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.NAME,"TargetContent")))
+def get_attendance(driver):
+	try:
+		#elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"pthdr2home")))
+		#elem.click()
+	
+		#elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.NAME,"TargetContent")))
+	#var=elem.get_attribute('src')
+#		driver.get(var)
+	
+	#	elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"DERIVED_SSS_SCR_SSS_LINK_ANCHOR7")))
+	#	elem.click()
+	
+		elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"SSR_DUMMY_RECV1$sels$0")))
+		elem.click() 
+		elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"DERIVED_AA2_B_ATTEND_ROSTER")))
+		elem.click()
+		elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"CLASS_TBL_VW$scroll$0")))
+		driver.save_screenshot("screen.png")
+		if sys.platform == 'linux2':
+			subprocess.call(["xdg-open","screen.png"])
+		else:
+			os.startfile(file)
+		driver.close()
+	except selenium.common.exceptions.TimeoutException:
+		print("Timed out. Terminating")
+		driver.close()
 
-#elem=driver.find_element_by_name('TargetContent')
-	var=elem.get_attribute('src')
-	driver.get(var)
 
-	elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"DERIVED_SSS_SCR_SSS_LINK_ANCHOR7")))
-
-#elem=driver.find_element_by_id('DERIVED_SSS_SCR_SSS_LINK_ANCHOR7')
-	elem.click()
-
-	elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"SSR_DUMMY_RECV1$sels$0")))
-
-#elem=driver.find_element_by_id('SSR_DUMMY_RECV1$sels$0')
-	elem.click() 
-
-	elem=WebDriverWait(driver,120).until(EC.presence_of_element_located((By.ID,"DERIVED_AA2_B_ATTEND_ROSTER")))
-
-#elem=driver.find_element_by_id('DERIVED_AA2_B_ATTEND_ROSTER')
-	elem.click()
-except TimeoutException as e:
-	print("Timed out. Retrying")
-	driver.close()
+main()
